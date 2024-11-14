7ebcdc63d6ad65940283bc0cee244625d86b8cb9
Paul Cabot <paul.claude.cabot@gmail.com>
2024-11-14 16:35:45 +0100

    Delete done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,34 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmarks_params)
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.movie = @movie
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    redirect_to list_path(@list), status: :see_other
+  end
+
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment)
+  end
+
 end
