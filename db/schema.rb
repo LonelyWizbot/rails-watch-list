fa6d0ff94c30be960f4698dcd94340c33bd6fbe8
Paul Cabot <paul.claude.cabot@gmail.com>
2024-11-15 12:51:16 +0100

    Fixed my mistakes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_14_103134) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_15_115023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_103134) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
